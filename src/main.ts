6d133a9b3f1132f9f852309418f1c5bb9d69c13f
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2025-01-05 22:06:48 +0900

    setting graphql redis

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,57 @@
 import { NestFactory } from '@nestjs/core'
 
 import { CoreModule } from './core/core.module'
+import { ConfigService } from '@nestjs/config'
+import * as cookieParser from "cookie-parser"
+import { ValidationPipe } from '@nestjs/common'
+import * as session from 'express-session'
+import { ms, type StringValue } from './shared/utils/ms.util'
+import { parseBoolean } from './shared/utils/parse-boolean.util'
+import { RedisStore } from 'connect-redis'
+import { RedisService } from './core/redis/redis.service'
 
 async function bootstrap() {
 	const app = await NestFactory.create(CoreModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	const config = app.get(ConfigService)
+	const redis = app.get(RedisService)
+
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+	// app.use(config.getOrThrow<string>('GRAPHQL_PREFIX'))
+
+	app.useGlobalPipes(new ValidationPipe({
+		transform: true
+	}))
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+				httpOnly: parseBoolean(
+					config.getOrThrow<string>('SESSION_HTTP_ONLY')
+				),
+				secure: parseBoolean(
+					config.getOrThrow<string>('SESSION_SECURE')
+				),
+				sameSite: 'lax'
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			})
+		})
+	)
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposedHeaders: ["set-cookie"]
+	})
+
+	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'))
 }
 bootstrap()
